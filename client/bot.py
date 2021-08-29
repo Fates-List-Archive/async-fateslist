2045a4f043b6c3d7589ea0d419096158799b3cc9
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-08-29 14:20:30 +0530

    basic init

---
+++ client/bot.py
@@ -0,0 +1,7 @@
+
+class BotClient:
+    def __init__(self, token: str):
+        return
+    
+    def __str__(self):
+        return '<Fates Bot-Client Connection>'
---
+++ client/user.py
@@ -0,0 +1,7 @@
+
+class UserClient:
+    def __init__(self, token: str):
+        return
+    
+    def __str__(self):
+        return '<Fates User-Client Connection>'
