1aec9bcb02f16b3c10b75d1a74a3abefed1f4fd6
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-08-31 13:38:52 +0530

    reorganisation

--- async_fateslist/client/user.py
+++ async_fateslist/client/user.py
@@ -12,12 +12,13 @@ class UserClient:
         Enum Reference: https://apidocs.fateslist.xyz/structures/enums.autogen
     '''
     
-    def __init__(self, token: str, api_ver: Optional[int] = 2, beta: Optional[bool] = False):
+    def __init__(self, token: str, api_ver: Optional[int] = 2, beta: Optional[bool] = False, retry: Optional[bool] = False):
         if api_ver not in [2,3]:
             raise WrongApiVersionError
         self.token = token
         self.api_ver = api_ver
         self.beta = beta
+        self.retry = retry
     
     def __str__(self):
         return '<Fates User-Client Connection>'
