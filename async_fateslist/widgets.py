fb0c49939fb883631afb2f3c3856aaed151ddcb5
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-12-02 23:21:41 +0530

    Update widgets.py

--- async_fateslist/widgets.py
+++ async_fateslist/widgets.py
@@ -25,26 +25,14 @@ class Widgets:
     
     def download(self, opts: WidgetOptions) -> aiohttp.ClientResponse:
         '''
-        Get Widget
-
-        Returns a widget
-        
-        format - What format widget to return
-
-        cd - A custom description you wish to set for the widget
-
-        full_desc - If this is set to true, the full description will be used, otherwise, only the first 25 characters will be used
-
-        no_cache - If this is set to true, cache will not be used but will still be updated. 
-        If using cd, set this option to true and cache the image yourself.
-        Note that no_cache is slow and may lead to ratelimits and/or your got being banned if used excessively
+        Downloads a widget
         '''
+        
         return await BaseHTTP(
-                api_token="", 
-                api_ver=self.api_ver
-            ).request(
-                method="GET", 
-                endpoint=self.url(opts),
-                retry=self.retry
-            )
+            api_token="", 
+            api_ver=self.api_ver
+        ).request(
+            method="GET", 
+            endpoint=self.url(opts),
+            retry=self.retry
         )
